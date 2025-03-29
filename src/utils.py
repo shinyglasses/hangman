984fa10ca6633f9fbed3a692cb3f9ea4c14099b3
shinyglasses <crunchytvtable@gmail.com>
2025-03-28 21:40:21 -0400

    set up

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+
+def render_text(text, font, font_size, color, position):
+    font = pygame.font.Font(font, font_size)
+    text_surface = font.render(text, True, color)
+    screen.blit(text_surface, position)
