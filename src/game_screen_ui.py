c999e344e610d49c079be5808dc9e4f30122be2c
shinyglasses <crunchytvtable@gmail.com>
2025-04-18 21:47:23 -0400

    Combine exit screen, refactor, and fix exit issue

--- src/game_screen_ui.py
+++ src/game_screen_ui.py
@@ -138,6 +138,7 @@ from randomwords import get_random_word
 from hangman import Hangman
 from gameplay_elements import Gameplay_Elements
 from start_screen import StartScreen
+from endscreen import EndScreen
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -147,15 +148,18 @@ font = os.path.join('resources', 'Roboto-Regular.ttf')
 running = True
 startscreen = StartScreen(screen)
 game_won = False  # Flag to track if the game has been won
+word = get_random_word()  # Randomly selects a word each time
+hangman = Hangman(word)
+gameplay_elements = Gameplay_Elements()
+print(word)
+
+game_state = 'Start Screen' #can be start screen, end screen, or game screen
 
 # Main Game Loop
 while running:
-    # Start Screen Loop
-    showing_start_screen = True
-    while showing_start_screen:
+    while game_state == 'Start Screen':
         screen.fill('white')
         startscreen.display_all_elements()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -163,46 +167,41 @@ while running:
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if startscreen.button_rect.collidepoint(event.pos):
                     # If clicked inside the button rect, move on to game
-                    showing_start_screen = False
+                    game_state = 'Game Screen'
                     break
-
         pygame.display.flip()
 
-    # Initialize game variables
-    word = get_random_word()  # Randomly selects a word each time
-    hangman = Hangman(word)
-    gameplay_elements = Gameplay_Elements()
-    print(word)
-    game_won = False  # Reset the win condition every new game
-
-    # Game Loop
-    while not game_won:
+    while game_state == 'Game Screen':
         screen.fill(screen_color)
         hangman.display()
         gameplay_elements.create_underlines(word)
-
         gameplay_elements.input_answer()
-
         # Display user letter in the textbox
         utils.render_text(gameplay_elements.letter, font, 30, 'black', (370, 350))
-
         if gameplay_elements.show_invalid_input_message:
             utils.render_text('Invalid input. Only letters allowed', font, 40, 'black', (120, 400))
-
         # Check if the player has guessed all letters correctly
         if gameplay_elements.display_correct_guesses(word):
-            game_won = True
-
+            game_state = 'Start Screen'
+        if hangman.limb_count >= len(hangman.body_parts):
+            game_state = 'End Screen'
+            hangman.limb_count = 0
+          
         # Handle user input during gameplay
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                game_won = True  # Stop the game if user closes the window
+                game_state = 'End Screen'  # Stop the game if user closes the window
             gameplay_elements.handle_user_input(event, word, gameplay_elements.letter)
 
         pygame.display.flip()
 
     # Reset the game when won, and go back to the start screen
-    if game_won:
-        startscreen.display_all_elements()
+    while game_state == 'End Screen':
+        EndScreen.draw_end_screen()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                game_state = 'Start Screen'
         pygame.display.flip()
--- src/hangman.py
+++ src/hangman.py
@@ -22,13 +22,11 @@ class Hangman:
     pygame.draw.line(screen, 'black', (900, 50), (1000, 50), 20) #top bar of hanging thing
     pygame.draw.line(screen, 'black', (900, 41), (900, 450), 20) #long vertical bar of hanging thing
     pygame.draw.line(screen, 'black', (870, 450), (930, 450), 20) #bottom bar of hanging thing
+
     for limb in range(self.limb_count):
-        print(limb)
         part = self.body_parts[limb]
         if part['type'] == 'circle':
             pygame.draw.circle(screen, 'black', part['pos'], part['radius'])
         elif part['type'] == 'line':
             pygame.draw.line(screen, 'black', part['start_pos'], part['end_pos'], part['width'])
-    if self.limb_count > len(self.body_parts):
-      EndScreen.display()
-      self.limb_count = 0
+  
